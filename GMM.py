3f7a3500da2cfd7cfa2b66080f875c9cd0fa5dd4
SnkhchyanV <82439632+SnkhchyanV@users.noreply.github.com>
2023-04-25 17:33:20 +0400

    Update 2.0, alpha version

--- GMM.py
+++ GMM.py
@@ -1,5 +1,4 @@
 from sklearn.cluster import KMeans
-import pandas as pd
 import numpy as np
 
 
@@ -8,6 +7,12 @@ class GMM:
         self.k = k
         self.method = method
         self.max_iter = max_iter
+        self.mean_arr = None
+        self.cov_arr = None
+        self.pi_arr = None
+        self.gamma_mtrx = None
+        self.loss = None
+        self.tol = tol
 
     def init_centers(self, X):
         if self.method == 'random_mean_std':
@@ -41,60 +46,71 @@ class GMM:
 
     def fit(self, X):
         self.mean_arr, self.cov_arr, self.pi_arr = self.init_centers(X)
-        self.loss = self.loss(...)
+        # self.loss = self.loss(...)
 
         for _ in range(self.max_iter):
-            self.gamma_mtrx = self.expectation(X)
-            self.mean_arr, self.cov_arr, self.pi_arr = self.maximization(X)
+            self.gamma_mtrx = self.__expectation(X)
+            self.mean_arr, self.cov_arr, self.pi_arr = self.__maximization(X)
 
-            loss = self.loss(...)
-            if loss == self.loss:  # add tolerance comparison
+            loss = self.loss_NLL(X, self.mean_arr, self.cov_arr, self.pi_arr)
+            if loss <= self.tol:  # add tolerance comparison
                 break
+
             self.loss = loss
-            self.mean_arr = mean_arr
-            self.cov_arr = cov_arr
-            self.pi_arr = pi_arr
 
-    def loss(self, X, mean, cov, pi):
-        pass
+    def loss_NLL(self, X, mean_arr, cov_arr, pi_arr):
+        log_sum = 0
+        for i in range(X.shape[0]):
+            summ = 0
+            for k in range(self.k):
+                summ += pi_arr[k] * self.__pdf(X, mean_arr[k], cov_arr[k])
+                # summ = np.sum((pi_arr * self.__pdf(X, mean_arr, cov_arr)),axis=0)
+            log_sum += np.log(summ)
 
-    def pdf(self, x, mean, cov):
+        return -log_sum
+
+    def __pdf(self, x, mean, cov):
         proba = (1 / (cov * np.sqrt(2 * np.pi))) * np.exp((-1 / 2) * (((x - mean) / cov) ** 2))
         return proba
 
-    def expectation(self, X):
+    def __expectation(self, X):
         n, m = X.shape
         gamma_mtrx = np.zeros((n, self.k))
 
         for k in range(self.k):
             for i in range(n):
-                gamma_mtrx[i][k] = (self.pi_arr[k] * self.pdf(X[i], self.mean_arr[k], self.cov_arr[k]))
+                gamma_mtrx[i][k] = (self.pi_arr[k] * self.__pdf(X[i], self.mean_arr[k], self.cov_arr[k]))
                 summ = 0
                 for j in range(self.k):
-                    summ += (self.pi_arr[j] * self.pdf(X[i], self.mean_arr[j], self.cov_arr[j]))
+                    summ += (self.pi_arr[j] * self.__pdf(X[i], self.mean_arr[j], self.cov_arr[j]))
                 gamma_mtrx[i][k] /= summ
 
         return gamma_mtrx
 
-    def maximization(self, X):
+    def __maximization(self, X):
+        mean_arr = np.zeros((self.k, X.shape[1]))
+        cov_arr = np.zeros((self.k, X.shape[1], X.shape[1]))
+        pi_arr = np.zeros((self.k, 1))
+        Nk = np.sum(self.gamma_mtrx, axis=0)
+        Nk /= X.shape[0]
+        # mean_arr = np.sum ((self.gamma_mtrx.dot(X)) , axis=0 )
         for k in range(self.k):
-            Nk = 0
-            new_mean_sum = 0
-            new_sigma_sum = 0
             for i in range(X.shape[0]):
-                Nk += self.gamma_mtrx[i][k]
-                new_mean_sum += (self.gamma_mtrx[i][k] * X[i])
+                mean_arr[k] += self.gamma_mtrx[i][k]*X[i]
+            mean_arr[k] /= Nk[k]
 
-            self.mean_arr[k] = new_mean_sum/Nk
-
-
-
+            for i in range(X.shape[0]):
+                cov_arr[k] = self.gamma_mtrx[i][k] * ((X[i] - mean_arr[k]).dot((X[i]-mean_arr[k]).T))
+            cov_arr[k] /= Nk[k]
+            pi_arr[k] = Nk[k]/X.shape[0]
 
         return mean_arr, cov_arr, pi_arr
 
     def predict(self, X):
-        return
+        prediction_proba = self.__expectation(X)
+        y = np.argmax(prediction_proba, axis=0)
+        return y
 
     def predict_proba(self, X):
         # return predictions using expectation function
-        return
+        return self.__expectation(X)
