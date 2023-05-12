23d6f99bced3e422ed4df8488aa4bb80eb118940
SnkhchyanV <82439632+SnkhchyanV@users.noreply.github.com>
2023-05-12 21:35:10 +0400

    Beta 1.1 , cosmetic update

--- SpectralClustering.py
+++ SpectralClustering.py
@@ -11,39 +11,27 @@ class SpectralClustering:
         self.__kernel_params = kernel_params
         self.__affinity_mtrx = None
         self.labels = None
-        self.vector = None
 
     def fit(self, X):
 
         if self.__affinity == 'rbf':
             affinity_mtrx = self.__init_affinity_rbf(X, self.__kernel_params)
 
-
         d = np.sum(affinity_mtrx, axis=0)
         L = np.zeros_like(affinity_mtrx)
         L = L - affinity_mtrx
         for i in range(len(d)):
-            for j in range(len(d)):
-                if i==j:
-                    L[i][j] = d[i]
+            L[i][i] = d[i]
 
         self.__affinity_mtrx = L
-
         eig_w, eig_v = np.linalg.eigh(self.__affinity_mtrx)
-        print(eig_v)
-
         h_mtrx = eig_v[:, 0:self.__n_clusters]
-        #h_mtrx = np.hstack((np.ones((X.shape[0], 1)), h_mtrx))
-        print(h_mtrx)
-        self.vector = h_mtrx
+
         if self.__assign_labels == 'kmeans':
             KMeans_clustering = KMeans(self.__n_clusters, n_init='auto')
             self.labels = KMeans_clustering.fit_predict(h_mtrx)
 
         return self
-
-    def get_self_vectors(self):
-        return self.vector
 
     def fit_predict(self, X):
         return self.fit(X).labels
